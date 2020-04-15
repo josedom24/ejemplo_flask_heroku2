a2d3eb9f4ed9ffafa3548e9e2ff3ad52306f0fe3
José Domingo Muñoz <josedom24@gmail.com>
2020-04-15 11:17:02 +0200

    requirements

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template,abort
 app = Flask(__name__)	
 
 notas = [
-    {"id":1,"nombre":"Paco Pérez","curso":"4º","nota":"6.25"}, 
+    {"id":1,"nombre":"Francisco Pérez","curso":"4º","nota":"6.25"}, 
     {"id":2,"nombre":"Manuel Rodríguez","curso":"3º","nota":"5.00"},
     {"id":3,"nombre":"María Roldán","curso":"2º","nota":"7.15"}]
 
